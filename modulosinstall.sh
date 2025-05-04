5c01a8cc569e69aa97fa6abadc791515acfb2dcf
Ar7hurZ7 <104260152+Ar7hurZ7@users.noreply.github.com>
2025-05-04 00:49:58 -0300

    Update modulosinstall.sh

--- modulosinstall.sh
+++ modulosinstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 rm -f atlasdata.sh atlascreate.sh atlasteste.sh atlasremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
-wget -O atlascreate.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/atlascreate.sh"
-wget -O add.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/add.sh"
-wget -O remsinc.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/remsinc.sh"
-wget -O addsinc.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/addsinc.sh"
-wget -O rem.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/rem.sh"
-wget -O atlasteste.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/atlasteste.sh"
-wget -O addteste.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/addteste.sh"
-wget -O atlasremove.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/atlasremove.sh"
-wget -O delete.py "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/delete.py"
-wget -O atlasdata.sh "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/atlasdata.sh"
-wget -O sincronizar.py "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/sincronizar.py"
+wget -O atlascreate.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/atlascreate.sh"
+wget -O add.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/add.sh"
+wget -O remsinc.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/remsinc.sh"
+wget -O addsinc.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/addsinc.sh"
+wget -O rem.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/rem.sh"
+wget -O atlasteste.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/atlasteste.sh"
+wget -O addteste.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/addteste.sh"
+wget -O atlasremove.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/atlasremove.sh"
+wget -O delete.py "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/delete.py"
+wget -O atlasdata.sh "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/atlasdata.sh"
+wget -O sincronizar.py "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/sincronizar.py"
 chmod 777 atlascreate.sh add.sh remsinc.sh addsinc.sh rem.sh atlasteste.sh addteste.sh atlasremove.sh delete.py atlasdata.sh sincronizar.py
 apt install dos2unix
 dos2unix rem.sh
-wget "https://raw.githubusercontent.com/atlaspaineL/atlasPainel/main/verificador.py" -O verificador.py 
+wget "https://raw.githubusercontent.com/Ar7hurZ7/modulosatlas/refs/heads/main/verificador.py" -O verificador.py 
 python3 verificador.py
